32ff6fc525979344598f6d01d264b8bb8500fda3
Yasshi-cookie <y.teruyacookie@gmail.com>
2023-09-21 00:34:52 +0900

    [update]プロパティの型を変更&プロパティのリセットを追加

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,21 +2,31 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\Stringable;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Title('Todo list')]
 class TodoList extends Component
 {
-    public array $todoList = [];
+    public Collection $todoList;
 
-    public string $todo = '';
+    public string $todo;
+
+    public function mount(): void
+    {
+        $this->todoList = collect();
+        $this->todo = '';
+    }
 
     public function add()
     {
-        $this->todoList[] = $this->todo;
+        $this->todoList->push($this->todo);
 
-        $this->todo = '';
+        // todoプロパティをリセット
+        // mountメソッドが呼び出される前の状態に戻す。※mountメソッドの中では使えない。
+        $this->reset('todo');
     }
 
     public function render()
